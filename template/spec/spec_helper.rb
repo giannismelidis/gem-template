85ac3bc174d9e55ebfb72c7420afade7d7479e99
Giannis Melidis <giannismelidis@olery.com>
2015-10-31 12:03:45 +0100

    Added default rspec configuration.

--- template/spec/spec_helper.rb
+++ template/spec/spec_helper.rb
@@ -1,2 +1,17 @@
 require 'GEM_NAME'
 include GEM_NAMESPACE
+
+require_relative 'support/simplecov' if ENV['COVERAGE']
+
+RSpec.configure do |config|
+  config.color = true
+  config.use_transactional_examples = true
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
